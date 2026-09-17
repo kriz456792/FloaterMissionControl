8ef15a806a7a54102dac2faa7976f451418b1be8
Chris <32822405+kriz456792@users.noreply.github.com>
2026-09-17 00:38:42 -0700

    Configuration Updates

--- build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_FloaterMissionControl_FILE C:/Users/kriz4/OneDrive/Qt6 Projects/FloaterMissionControl/build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/FloaterMissionControl.exe)
-set(__QT_DEPLOY_TARGET_FloaterMissionControl_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_FloaterMissionControl_RUNTIME_DLLS C:/Qt/6.10.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.10.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.10.1/mingw_64/bin/Qt6Core.dll)
